f3f66d3ccd9abe900252d85bbd274a21701f1443
Pixlforge <celien@pixlforge.ch>
2019-05-29 19:23:45 +0200

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    title: pkg.name,
+    title: "Foreskin Restoration Calculator",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
